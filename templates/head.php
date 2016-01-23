df65fb07ca42db9c260b0e398680baeb2b17b900
Manu <emvincent@nsup.org>
2016-01-22 23:10:16 -0800

    remove GA for now

--- templates/head.php
+++ templates/head.php
@@ -24,9 +24,10 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
     <?php wp_head(); ?>
-    <script type="text/javascript">
+  <!--   <script type="text/javascript">
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-55053509-1', 'auto');
 		ga('send', 'pageview');
     </script>
+  -->
   </head>
